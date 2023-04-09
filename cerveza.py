9b33ebb43ac53ad40f7f9090d784257db3201db1
hardevkoder <haroldvaldes@yahoo.com>
2023-04-08 20:48:15 -0500

    08 ABR 2023 - Creacion del Repositorio

---
+++ cerveza.py
@@ -0,0 +1,5 @@
+"""PROYECTO NOMBRE DE CERVEZA"""
+print('El Nombre Generado para tu cerveza es:\nCerveza "' +
+      input('Cuál es el color base de tu Negocio?: ') + ' ' +
+      input('Cual es tipo de cerveza (Digita el Tipo):\n1-Light\n2-Seca\n3-Negra\n') +
+      '"')
